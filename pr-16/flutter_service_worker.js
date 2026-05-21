480ebe733524a80de64247a97d29d1b3b1cfcc1b
jounile <jounile@users.noreply.github.com>
2026-05-21 13:50:33 +0000

    Deploying to gh-pages from @ jounile/NollaApp@c0cb3879b0af95f05098f9cb1abb30e2325687a1 🚀

--- pr-16/flutter_service_worker.js
+++ pr-16/flutter_service_worker.js
@@ -31,9 +31,9 @@ const RESOURCES = {"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "canvaskit/canvaskit.js": "738255d00768497e86aa4ca510cce1e1",
 "canvaskit/skwasm.wasm": "4051bfc27ba29bf420d17aa0c3a98bce",
-"flutter_bootstrap.js": "207687ed78f742b67bc1b52833747fdb",
+"flutter_bootstrap.js": "beacadfe5ab1704cd57a34419b6c9f92",
 "manifest.json": "431eaa3225ccc686218bccf93eb161d0",
-"main.dart.js": "d852a3caa09b762c0681a406a889ac0d"};
+"main.dart.js": "08ff16926eeee566b4d11db5bc032e04"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
