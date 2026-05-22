42263f5a91433bb0067c7c8eb54cea1c26f39ce6
jounile <jounile@users.noreply.github.com>
2026-05-22 07:40:17 +0000

    Deploying to gh-pages from @ jounile/NollaApp@715d7eeb2483b7fad6e3053a59ca9c470d32ae84 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1029111074"
+    serviceWorkerVersion: "3105068451"
   }
 });
