c55755f235f7aa551a9a8495244c43c4cf27b1e0
jounile <jounile@users.noreply.github.com>
2026-05-31 20:34:33 +0000

    Deploying to gh-pages from @ jounile/NollaApp@99c549ceb730ea72c7d6532366b6d8747e86930b 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,12 +19,12 @@ const RESOURCES = {"version.json": "82929128883a0420cd8107cb2abc09ce",
 "canvaskit/canvaskit.js.symbols": "74a84c23f5ada42fe063514c587968c6",
 "canvaskit/canvaskit.wasm": "9251bb81ae8464c4df3b072f84aa969b",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "9409132f30c0331edf53b7b6371d707a",
+"flutter_bootstrap.js": "c85b9e3757ba75e0f3d719376d68a07c",
 "manifest.json": "431eaa3225ccc686218bccf93eb161d0",
 "index.html": "d08519e948a3f18b5d8ebe2948673788",
 "/": "d08519e948a3f18b5d8ebe2948673788",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "87f3ed662fb36152b0cd2b207234ba86",
+"main.dart.js": "0059d3f3561bcd0190a34346153999d6",
 "assets/NOTICES": "2c386b8b733ec0f16e6d7c7773d29b64",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
@@ -33,7 +33,7 @@ const RESOURCES = {"version.json": "82929128883a0420cd8107cb2abc09ce",
 "assets/AssetManifest.json": "96d47c011a5a5aa71f9631b60049eb4e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"assets/fonts/MaterialIcons-Regular.otf": "5ba9867ea010dec05cb655be4732b9fd"};
+"assets/fonts/MaterialIcons-Regular.otf": "6a3668bc1e5f23c8fa39fd728390ceb2"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
